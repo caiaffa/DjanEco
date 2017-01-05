ca6ad1348e3dbca55eaa15c4f59b5411f7eb7dc8
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-01-05 10:56:13 -0200

    Ajustes

--- DjanEco/settings.py
+++ DjanEco/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libs
+    'widget_tweaks',
     # apps
     'product',
     'accounts',
@@ -52,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # apps
+    'checkout.middleware.cart_item_middleware',
 ]
 
 ROOT_URLCONF = 'DjanEco.urls'
