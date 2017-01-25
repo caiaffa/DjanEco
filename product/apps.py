2d7be3c52fe9789a7c6de91a02f7c602e40f9a36
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-01-25 17:34:25 -0200

    Boostrap

--- product/apps.py
+++ product/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from watson import search as watson
 
 class ProductConfig(AppConfig):
     name = 'product'
     verbose_name = 'Produto'
+
+    def ready(self):
+        Product = self.get_model('Product')
+        watson.register(Product)
